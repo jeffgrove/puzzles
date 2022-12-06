b29ac32d17399b95dda03def9898576f9ef9fd02
Jeff Grove <jeff@dashplatform.com>
2022-12-05 16:05:31 -0800

    step2: add in v4 query and optimization

--- events_sql/event_conflict_tests.sql
+++ events_sql/event_conflict_tests.sql
@@ -27,3 +27,25 @@ CALL p_get_event_conflicts_v2(1, '2022-12-01 15:30', '2022-12-01 17:30');
 CALL p_get_event_conflicts_v3(1, '2022-12-01 15:30', '2022-12-01 17:30');
 
 
+-- 4) Candidate (X) encompassed by Event (A)
+CALL p_get_event_conflicts_v3(1, '2022-12-01 15:30', '2022-12-01 17:30');
+
+
+-- 5) Candidate (X) before Event (A)
+CALL p_get_event_conflicts_v3(1, '2022-12-01 13:30', '2022-12-01 14:30');
+
+
+-- 6) Candidate (X) after Event (A)
+CALL p_get_event_conflicts_v3(1, '2022-12-01 17:30', '2022-12-01 18:30');
+
+
+-- 7) End of Candidate (X) equal to start of Event (A)
+CALL p_get_event_conflicts_v3(1, '2022-12-01 15:00', '2022-12-01 16:00');
+
+
+-- 8) Start of Candidate (X) equal to end of Event (A)
+CALL p_get_event_conflicts_v3(1, '2022-12-01 17:00', '2022-12-01 18:00');
+
+
+-- 9) Candidate (X) exactly matches Event (A)
+CALL p_get_event_conflicts_v3(1, '2022-12-01 16:00', '2022-12-01 17:00');
---
+++ events_sql/p_get_event_conflicts_v4.sql
@@ -0,0 +1,23 @@
+-- Stored procedure to take input resourceID, startGMT and endGMT and check for event conflicts
+DROP PROCEDURE IF EXISTS p_get_event_conflicts_v4;
+
+DELIMITER $$
+
+CREATE PROCEDURE p_get_event_conflicts_v4(
+  IN input_resource_id INT(11) UNSIGNED,
+  IN input_start_gmt DATETIME,
+  IN input_end_gmt DATETIME
+    )
+BEGIN
+
+  SELECT input_start_gmt AS input_start, input_end_gmt AS input_end, events.*
+  FROM events
+  WHERE
+    resource_id = input_resource_id
+    AND input_start_gmt < end_gmt
+    AND input_end_gmt > start_gmt
+  LIMIT 1;
+
+END$$
+DELIMITER ;
+;
